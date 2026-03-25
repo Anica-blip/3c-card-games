723308d2365ad8eeee27a054d085a4d1a584b8e7
Anica <3c.innertherapy@gmail.com>
2026-03-25 16:17:23 +0000

    Update worker.js

--- worker/worker.js
+++ worker/worker.js
@@ -27,6 +27,7 @@
 
 const ALLOWED_ORIGINS = [
   'https://anica-blip.github.io',
+  'https://3c-card-games.vercel.app',
   'http://localhost:5500',
   'http://127.0.0.1:5500',
   'http://localhost:3000',
